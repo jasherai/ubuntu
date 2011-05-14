e76d8c0a0de83052f3dadf5ee43dfffd3107c4ad
Pritesh <pritesh@phatforge.com>
2011-05-14 13:13:59 +0100

    UEC WIP

--- config.sh
+++ config.sh
@@ -38,8 +38,28 @@
 "}
 
 # Fully-qualified pathname of VBoxGuestAdditions.iso.
-#: ${VBOX_GUEST_ADDITIONS:="/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso"}
-: ${VBOX_GUEST_ADDITIONS:="/usr/share/virtualbox/VBoxGuestAdditions.iso"}
+os=$(uname)
+if [ ${os} = "Linux" ]; then 
+  VBOX_GUEST_ADDITIONS="/usr/share/virtualbox/VBoxGuestAdditions.iso" 
+elif [ ${os} = "Darwin" ]; then
+  VBOX_GUEST_ADDITIONS="/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso"
+fi
+
+#### UEC BUILD SETTINGS
+## Build a vagrant box using the UEC image as a base. (Instead of the main server isos)
+##
+###
+## Enable UEC build
+: ${VBOX_UEC_BUILD:=1}
+# UEC Image to use. current/release: Use a daily build or Official release version. At time of writing ovf is only available for natty daily builds.
+: ${UEC_TYPE:="releases"}
+# Specify a specific date{.iteration} version to use. (Currently set to this as last few days haven't been generated)
+: ${UEC_VERSION:="alpha-3"}
+# Enable a python webserver for UEC cloud-meta files. (user-data, meta-data)
+: ${UEC_WEBSERVER:=1}
+## Preseed URI address. Default is set to the Host machine ip. This address can be set to a remote url (assuming your VM can access it.) Please see README.PXE
+: ${UEC_HOST:="http://10.0.2.2:8000"}
+: ${UEC_PATH:="web-root"}
 
 #### PXE BUILD SETTINGS
 ## You need a webserver to serve the preseed file. We currently use python's simpleHTTPServer module, but you can change the path to a full webserver if you want.
@@ -56,7 +76,7 @@
 : ${PRESEED_PATH:="web-root"}
 
 # apt-cacher-ng support. If you have a proxy running locally set the ip address here for apt to proxy requests. (10.0.2.2 is the default gateway ip which should be your host)
-#: ${PROXY_URL:="http://10.0.2.2:3142"}
+: ${PROXY_URL:="http://10.0.2.2:3142"}
 # remove apt-proxy config for boxification? To remove the apt config for proxy settings from the box itself
 # (i.e. only use it for the OS install not for subsequent vagrant vms afterwards. Do your colleagues run apt-proxy on their host machines?)
 : ${BOX_DISABLE_APT_PROXY:=1}
